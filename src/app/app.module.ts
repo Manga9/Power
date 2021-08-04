e9e5fa83a32a5d96d888d473ff68d3edec687bd1
Manga9 <ahmedmasrey9@gmail.com>
2021-08-04 02:26:13 +0200

    adding topProducts component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { TopCategoriesComponent } from './components/top-categories/top-categories.component';
+import { TopProductsComponent } from './components/top-products/top-products.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TopCategoriesComponent } from './components/top-categories/top-categori
     HeaderComponent,
     NavbarComponent,
     HomeComponent,
-    TopCategoriesComponent
+    TopCategoriesComponent,
+    TopProductsComponent
   ],
   imports: [
     BrowserModule,
