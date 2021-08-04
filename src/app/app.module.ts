d6c18c6046822b153e5124d0ef00c763375a57a5
Manga9 <ahmedmasrey9@gmail.com>
2021-08-04 13:13:38 +0200

    make some changes in style and responsive in Home Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { TopCategoriesComponent } from './components/top-categories/top-categories.component';
 import { TopProductsComponent } from './components/top-products/top-products.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { TopProductsComponent } from './components/top-products/top-products.com
     NavbarComponent,
     HomeComponent,
     TopCategoriesComponent,
-    TopProductsComponent
+    TopProductsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
